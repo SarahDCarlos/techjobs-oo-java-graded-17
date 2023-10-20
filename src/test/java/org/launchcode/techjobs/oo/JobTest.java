725e74153fad562ed60015f89e936640d2c3ee6b
Sarah Carlos <SarahDCarlos@outlook.com>
2023-10-19 19:41:06 -0500

    Finished Assignment!

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -41,13 +41,50 @@ public class JobTest {
         Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
                 new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
+
+        String expected = "\n" +
+                "ID: " + job.getId() + "\n" +
+                "Name: Product tester" + "\n" +
+                "Employer: ACME" + "\n" +
+                "Location: Desert" + "\n" +
+                "Position Type: Quality control" + "\n" +
+                "Core Competency: Persistence" + "\n";
+
+        assertEquals(expected, job.toString());
+
     }
+
 
     @Test
     public void testToStringHandlesEmptyField(){
         Job testJob = new Job("Product tester", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
         String text = testJob.toString();
         assertTrue(text.contains("Data not available"));
+
+    }
+
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        String expected = System.lineSeparator();
+//                "ID: " + job.getId() + System.lineSeparator() +
+//                "Name: Product tester" + System.lineSeparator() +
+//                "Employer: ACME" + System.lineSeparator() +
+//                "Location: Desert" + System.lineSeparator() +
+//                "Position Type: Quality control" + System.lineSeparator() +
+//                "Core Competency: Persistence" + System.lineSeparator();
+//                "\r\n"; // Ending new line
+
+
+        int length = job.toString().length()-2;
+        System.out.println(length);
+        assertEquals(expected, job.toString().charAt(0));
+        assertEquals(expected, job.toString().charAt(118));
+
+
     }
 
     }
